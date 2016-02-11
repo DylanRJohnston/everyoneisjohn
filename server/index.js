fcf18678ed60d9af42646f99c88c9bce783af780
Dylan R. Johnston <Dylan.R.Johnston@gmail.com>
2016-02-12 01:41:59 +0800

    Added notifications index.

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 Meteor.startup(function () {
-  Players._ensureIndex({ gameId: 1});
-  Players._ensureIndex({ ownerId: 1});
-  Games._ensureIndex({ johnId: 1 });
+    Players._ensureIndex({gameId: 1});
+    Players._ensureIndex({ownerId: 1});
+    Games._ensureIndex({johnId: 1});
+    Notifications._ensureIndex({gameId: 1});
 });
