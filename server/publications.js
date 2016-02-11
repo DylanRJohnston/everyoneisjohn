4b6728f4d6ad91aa7c2a6671db3875b7a5575f70
Dylan R. Johnston <Dylan.R.Johnston@gmail.com>
2016-02-11 19:07:32 +0800

    Added player publications

--- server/publications.js
+++ server/publications.js
@@ -1,6 +1,43 @@
 Meteor.publish('games', function(gameId){
-    check(this.userId, String);
-    check(gameId, String);
+
+    //audit-arguments-check package doesn't understand the try catch block.
+    check(arguments, Match.Any);
+    try {
+        check(this.userId, String);
+        check(gameId, String);
+    } catch (e) {
+        this.ready();
+        return;
+    }
 
     return Games.find({_id: gameId});
+});
+
+Meteor.publish('allPlayers', function(gameId) {
+
+    check(arguments, Match.Any);
+    try {
+        check(this.userId, String);
+        check(gameId, String);
+    } catch(e) {
+        this.ready();
+        return;
+    }
+
+    let projection = this.userId === Games.findOne({_id: gameId}).ownerId ? {} : {fields: {obsessions: 0}};
+
+    return Players.find({gameId: gameId}, projection);
+});
+
+Meteor.publish('myPlayers', function(gameId) {
+
+    check(arguments, Match.Any);
+    try {
+        check(this.userId, String);
+        check(gameId, String);
+    } catch(e) {
+        this.ready();
+        return;
+    }
+    return Players.find({gameId: gameId, ownerId: this.userId});
 });
